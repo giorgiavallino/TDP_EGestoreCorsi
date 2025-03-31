08a36e042d08ebc779cf7e1e6c6be17e8cbfc9f8
Giuseppe Averta <g.averta3@gmail.com>
2025-03-31 11:56:13 +0200

    lezione del 31/03/2025

--- model/model.py
+++ model/model.py
@@ -1,3 +1,15 @@
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
         pass
+
+    def getCodins(self):
+        return DAO.getCodins()
+
+    def getAllCorsi(self):
+        return DAO.getAllCorsi()
+
+    def getCorsiPd(self, pd):
+        return DAO.getCorsiPD(pd)
